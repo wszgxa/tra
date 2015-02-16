6305f038a7498a3faf354b92ac83cae3243ee229
wszgxa <z585819@qq.com>
2015-02-16 22:00:34 +0800

    修改部分bug

--- settings.js
+++ settings.js
@@ -2,5 +2,5 @@
 module.exports = {
   apiKey: 'cYY6bWTZG0KgcRSDSGZFifRb',
   version: '0.0.1',
-  help:'帮助：\n      输入tran 单词  可以单词翻译\n      输入tran -h调用帮助文档\n      输入tran -v    返回版本号'
+  help:'帮助：\n      输入tra 单词  可以单词翻译\n      输入tra -h调用帮助文档\n      输入tra -v    返回版本号'
 };

6e40d39e7e125d28db03f8b5b154671f438bb440
wszgxa <z585819@qq.com>
2015-02-16 22:02:26 +0800

    修改bug

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //参数设置文件
 module.exports = {
   apiKey: 'cYY6bWTZG0KgcRSDSGZFifRb',
-  version: '0.0.1',
+  version: '0.0.5',
   help:'帮助：\n      输入tra 单词  可以单词翻译\n      输入tra -h调用帮助文档\n      输入tra -v    返回版本号'
 };

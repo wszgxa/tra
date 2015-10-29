4340ab672904b93de93b6eb873db4b6fc1a2103a
zhaoguoxu <zhaoguoxu@youmi.net>
2015-10-29 18:15:56 +0800

    fix

--- settings.js
+++ settings.js
@@ -2,6 +2,6 @@
 module.exports = {
   apiKey: '1529685780',
   keyFrom: 'hilublog',
-  version: '0.1.0',
+  version: '0.1.1',
   help:'帮助：\n      输入tra 单词  可以单词翻译\n      输入tra -h调用帮助文档\n      输入tra -v    返回版本号'
 };

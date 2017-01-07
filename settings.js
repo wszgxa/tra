300183400753a9cf805b09d674ae855e4b47c7e9
hiluluke <z585819@qq.com>
2017-01-07 14:57:33 +0800

    update 加入commander.js

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 //参数设置文件
 module.exports = {
   apiKey: '1529685780',
-  keyFrom: 'hilublog',
-  version: '0.1.1',
-  help:'帮助：\n      输入tra 单词  可以单词翻译\n      输入tra -h调用帮助文档\n      输入tra -v    返回版本号'
+  keyFrom: 'hilublog'
 };
